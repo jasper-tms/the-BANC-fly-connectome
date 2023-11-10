349aa871558c57b183e2864a9fbc7bf202e97132
Jasper Phelps <jasper.s.phelps@gmail.com>
2023-11-10 14:07:43 -0500

    [fanc2banc] Update some ngl_info. WIP, doesn't quite work yet.

--- fanc/ngl_info.py
+++ fanc/ngl_info.py
@@ -9,37 +9,38 @@ client = auth.get_caveclient()
 info = client.info.get_datastack_info()
 
 ngl_app_url = info['viewer_site']
+#ngl_app_url = 'https://neuromancer-seung-import.appspot.com/'
 voxel_size = (info['viewer_resolution_x'],
               info['viewer_resolution_y'],
               info['viewer_resolution_z'])
 
-im = {'name': 'FANCv4',
-      # As of Jan 2023, path is 'precomputed://gs://lee-lab_female-adult-nerve-cord/alignmentV4/em/rechunked'
+im = {'name': 'BANC EM',
+      # As of Nov 2023, path is 'precomputed://gs://zetta_lee_fly_cns_001_alignment/v1_sharded'
       'path': info['aligned_volume']['image_source']}
 
 seg = {'name': 'segmentation proofreading',
-       # As of Aug 2022, path is 'graphene://https://cave.fanc-fly.com/segmentation/table/mar2021_prod'
+       # As of Nov 2023, path is 'graphene://middleauth+https://cave.fanc-fly.com/segmentation/table/wclee_fly_cns_001'
        'path': info['segmentation_source']}
 
 syn = {'name': 'postsynapses',
-       'path': 'precomputed://gs://lee-lab_female-adult-nerve-cord/alignmentV4/synapses/postsynapses_May2021'}
+       'path': '<not available>'}
 
 # The soma_table's flat_segmentation_source is the un-_verified layer
-#nuclei = {'name': 'nuclei_Mar2022',
+#nuclei = {'name': 'nuclei_TODO-DATE',
 #          'path': client.annotation.get_table_metadata(info['soma_table'])['flat_segmentation_source']
 # so instead hardcode the _verified layer:
 nuclei = {'name': 'nuclei (verified)',
-          'path': 'precomputed://gs://lee-lab_female-adult-nerve-cord/alignmentV4/nuclei/nuclei_seg_Mar2022_verified'}
+          'path': '<not available>'}
 
 view_options = dict(
-    position=[48848, 114737, 2690],
+    position=[113200, 106900, 3100],
     zoom_3d=6700,
     layout='xy-3d'
 )
 zoom_2d = 12
 
 
-outlines_layer = {'type': 'segmentation', 'mesh': 'precomputed://gs://lee-lab_female-adult-nerve-cord/alignmentV4/volume_meshes/meshes', 'objectAlpha': 0.1, 'hideSegmentZero': False, 'ignoreSegmentInteractions': True, 'segmentColors': { '1': '#bfbfbf', '2': '#d343d6' }, 'segments': [ '1', '2' ], 'hiddenSegments': [ '104633', '104634', '104635', '104636', '104637', '104638', '104639', '104640', '104641', '104642', '104643', '104644', '104645', '104646', '104647', '104648', '104649', '104650', '104651', '104652', '104653' ], 'skeletonRendering': { 'mode2d': 'lines_and_points', 'mode3d': 'lines' }, 'name': 'region outlines'}
+outlines_layer = {'type': 'segmentation', 'source': {'url': 'precomputed://gs://zetta_lee_fly_cns_001_kisuk/final/v2/volume_meshes', 'transform': {'matrix': [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0.9462, 0]], 'outputDimensions': {'x': [4e-9, 'm'], 'y': [4e-9, 'm'], 'z': [4.5e-8, 'm']}}, 'subsources': {'mesh': True}, 'enableDefaultSubsources': False}, 'tab': 'segments', 'meshSilhouetteRendering': 2, 'segments': ['1'], 'segmentDefaultColor': '#2a7fff', 'name': 'region outlines'}
 
 
 
@@ -60,10 +61,9 @@ def final_json_tweaks(state):
             layer['visible'] = False
             layer['shader'] = 'void main() { emitRGBA(vec4(1, 0, 1, toNormalized(getDataValue()))); }'
 
-    state['navigation']['zoomFactor'] = zoom_2d
+    state['crossSectionScale'] = zoom_2d
+    state['crossSectionOrientation'] = [0, 1, 0, 0]
     state.update({
         'gpuMemoryLimit': 4_000_000_000,
         'systemMemoryLimit': 4_000_000_000,
-        'concurrentDownloads': 64,
-        'jsonStateServer': 'https://global.daf-apis.com/nglstate/api/v1/post'
     })
