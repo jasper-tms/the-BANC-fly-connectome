f2f412f9b665558f0ccd8b9e5a076d13da5b9e28
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-03-05 23:05:39 +0100

    Bump version 0.2.0 -> 0.3.0

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ with open('requirements.txt', 'r') as f:
 
 setuptools.setup(
     name='banc',
-    version='0.2.0',
+    version='0.3.0',
     author='Jasper Phelps',
     author_email='jasper.s.phelps@gmail.com',
-    description="Tools for the GridTape-TEM dataset of an adult female fly's Brain And Nerve Cord",
+    description="Tools for the GridTape-TEM dataset of an adult female"
+                " fly's Brain And Nerve Cord",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/htem/FANC_auto_recon',
+    url='https://github.com/jasper-tms/the-BANC-fly-connectome',
     packages=['banc', 'banc.transforms'],
     package_data={'banc.transforms': ['transform_parameters/*.txt']},
     classifiers=[
