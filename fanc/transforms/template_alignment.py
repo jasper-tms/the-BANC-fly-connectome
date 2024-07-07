a19741fda5b34414fe9578cf3d7cfb15371c92f3
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-07-07 22:09:45 +0200

    Add alignment of BANC's brain to JRC2018_FEMALE

--- fanc/transforms/template_alignment.py
+++ fanc/transforms/template_alignment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Transform points and neurons between FANC and the 2018 Janelia VNC templates
+Transform points and neurons between the BANC and the 2018 Janelia templates
 """
 
 import os
@@ -11,8 +11,10 @@ import numpy as np
 from .. import template_spaces
 
 
-template_plane_of_symmetry_x_voxel = 329
-template_plane_of_symmetry_x_microns = 329 * 0.400
+vnc_template_plane_of_symmetry_x_voxel = 329
+vnc_template_plane_of_symmetry_x_microns = 329 * 0.400
+brain_template_plane_of_symmetry_x_voxel = 825
+brain_template_plane_of_symmetry_x_microns = 825 * 0.380
 
 
 def align_mesh(mesh, target_space='JRC2018_VNC_FEMALE', inplace=True):
@@ -36,6 +38,8 @@ def align_mesh(mesh, target_space='JRC2018_VNC_FEMALE', inplace=True):
       If true, replace the vertices of the given mesh object. If false, return
       a copy, leaving the given mesh object unchanged.
     """
+    raise NotImplementedError('This function has not yet been adapted from'
+                              ' FANC for use in the BANC.')
     import navis
     import flybrains
     if not inplace:
@@ -59,54 +63,40 @@ def align_mesh(mesh, target_space='JRC2018_VNC_FEMALE', inplace=True):
     if not inplace:
         return mesh
 
-def warp_points_FANC_to_template(points,
-                                 input_units='nanometers',
-                                 output_units='microns',
-                                 reflect=False):
+
+def warp_points_BANC_to_brain_template(points,
+                                       input_units='nanometers',
+                                       output_units='microns',
+                                       reflect=False):
     """
-    --- DEPRECATION NOTICE ---
-    This function has now been integrated into the more general
-      function navis.xform_brain (see https://github.com/navis-org/navis)
-      and it is preferred to use that instead of this functions in most cases.
-      For an example of using xform_brain, see other functions in this module.
-    ------
-
-    Transform point coordinates from FANC to the corresponding point
-    location in the 2018 Janelia Female VNC Template (JRC2018_VNC_FEMALE).
-
-    Formally, this transforms coordinates from FANCv3 (the version of
-    alignment used on CATMAID for manual tracing) to the template space,
-    but FANCv4 (the version of alignment used on Neuroglancer for
-    proofreading segmentation) is only slightly different from FANCv3,
-    such that if you call this function on point coordinates from
-    FANCv4, you'll still end up in essentially the correct location
-    within the VNC template.
+    Transform point coordinates from the BANC to the corresponding point
+    location in the 2018 Janelia Female Brain Template (JRC2018_FEMALE).
 
     Parameters
     ---------
     points (numpy.ndarray) :
-        An Nx3 numpy array representing x,y,z point coordinates in FANC
+        An Nx3 numpy array representing x,y,z point coordinates in the BANC
 
     input_units (str) :
         The units of the points you provided as an input. Set to 'nm',
         'nanometer', or 'nanometers' to indicate nanometers;
         'um', 'µm', 'micron', or 'microns' to indicate microns; or
         'pixels' or 'voxels' to indicate pixel indices within the
-        full-resolution FANC image volume, which has a pixel size of
-        (4.3, 4.3, 45) nm.
+        full-resolution BANC image volume, which has a pixel size of
+        (4, 4, 45) nm.
         Default is nanometers.
 
     output_units (str) :
         The units you want points returned to you in. Same set of
         options as for `input_units`, except that the pixel size of the
-        output space, JRC2018_VNC_FEMALE, is (0.4, 0.4, 0.4) µm.
+        output space, JRC2018_FEMALE, is (0.38, 0.38, 0.38) µm.
         Default is microns.
 
     reflect (bool) :
         Whether to reflect the point coordinates across the midplane of
-        the JRC2018_VNC_FEMALE template before returning them. This
+        the JRC2018_FEMALE template before returning them. This
         reflection moves points' x coordinates from the left to the
-        right side of the VNC template or vice versa, but does not
+        right side of the brain template or vice versa, but does not
         affect their y coordinates (anterior-posterior axis) or z
         coordinates (dorsal-ventral axis).
         Default is False.
@@ -114,13 +104,7 @@ def warp_points_FANC_to_template(points,
     Returns
     -------
     An Nx3 numpy array representing x,y,z point coordinates in
-    JRC2018_VNC_FEMALE, in units specified by `output_units`.
-
-    ------
-    Originally published in https://www.lee.hms.harvard.edu/phelps-hildebrand-graham-et-al-2021
-      and made available in that paper's repository located at:
-      https://github.com/htem/GridTape_VNC_paper/tree/main/template_registration_pipeline/register_EM_dataset_to_template
-    This function is a slight improvement on the published version.
+    JRC2018_FEMALE, in units specified by `output_units`.
     """
     import navis
     import flybrains
@@ -129,10 +113,12 @@ def warp_points_FANC_to_template(points,
 
     points = np.array(points, dtype=np.float64)
     if len(points.shape) == 1:
-        result = warp_points_FANC_to_template(np.expand_dims(points, 0),
-                                              input_units=input_units,
-                                              output_units=output_units,
-                                              reflect=reflect)
+        result = warp_points_BANC_to_brain_template(
+            np.expand_dims(points, 0),
+            input_units=input_units,
+            output_units=output_units,
+            reflect=reflect
+        )
         if result is None:
             return result
         else:
@@ -174,107 +160,82 @@ def warp_points_FANC_to_template(points,
     if input_units == 'microns':
         points *= 1000
     elif input_units == 'voxels':
-        points *= (4.3, 4.3, 45)
-
-    # This block of math looks mysterious but is explained in detail at
-    # https://github.com/htem/GridTape_VNC_paper/blob/main/template_registration_pipeline/register_EM_dataset_to_template/README.md
-    points -= (533.2, 533.2, 945)  # (1.24, 1.24, 2.1) vox at (430, 430, 450)nm/vox
-    points /= (430, 430, 450)
-    points *= (300, 300, 400)
-    points[:, 2] = 435*400 - points[:, 2]  # z flipping a stack with 436 slices
+        points *= (4, 4, 45)
 
     points /= 1000  # Convert nm to microns as required for this transform
     transform_params = os.path.join(
         os.path.dirname(__file__),
         'transform_parameters',
-        'TransformParameters.FixedFANC.txt'
+        'brain',
+        'BANC_to_template.txt',
     )
     # Do the transform. This requires input in microns and gives output in microns
     points = transformix.transform_points(points, transform_params)
 
-    if not reflect:  # The z flip above caused a reflection, so un-reflect
-        points[:, 0] = template_plane_of_symmetry_x_microns * 2 - points[:, 0]
+    if reflect:
+        points[:, 0] = brain_template_plane_of_symmetry_x_microns * 2 - points[:, 0]
 
     if output_units == 'nanometers':
         points *= 1000  # Convert microns to nm
     elif output_units == 'voxels':
-        points /= 0.4  # Convert microns to JRC2018_FEMALE_VNC voxels
+        points /= 0.38  # Convert microns to JRC2018_FEMALE voxels
 
     return points
 
 
-def warp_points_template_to_FANC(points,
-                                 input_units='microns',
-                                 output_units='nanometers',
-                                 reflect=False):
+def warp_points_brain_template_to_BANC(points,
+                                       input_units='microns',
+                                       output_units='nanometers',
+                                       reflect=False):
     """
-    --- DEPRECATION NOTICE ---
-    This function has now been integrated into the more general
-      function navis.xform_brain (see https://github.com/navis-org/navis)
-      and it is preferred to use that instead of this functions in most cases.
-      For an example of using xform_brain, see other functions in this module.
-    ------
-
-    Transform point coordinates from the 2018 Janelia Female VNC Template
-    (JRC2018_VNC_FEMALE) to the corresponding point location in FANC.
-
-    Formally, this transforms coordinates from the template space to
-    FANCv3 (the version of alignment used on CATMAID for manual tracing)
-    but FANCv4 (the version of alignment used on Neuroglancer for
-    proofreading segmentation) is only slightly different from FANCv3,
-    such that if you call this function and then input the results into
-    neuroglancer, you'll still end up in essentially the correct
-    location within FANC.
+    Transform point coordinates from the 2018 Janelia Female Brain Template
+    (JRC2018_FEMALE) to the corresponding point location in the BANC.
 
     Parameters
     ---------
     points (numpy.ndarray) :
         An Nx3 numpy array representing x,y,z point coordinates in the
-        2018 Janelia Female VNC Template, JRC2018_VNC_FEMALE.
+        2018 Janelia Female Brain Template, JRC2018_FEMALE.
 
     input_units (str) :
         The units of the points you provided as an input. Set to 'nm',
         'nanometer', or 'nanometers' to indicate nanometers; 'um', 'µm',
         'micron', or 'microns' to indicate microns; or 'pixels' or
-        'voxels' to indicate pixel indices within the JRC2018_VNC_FEMALE
-        image volume, which has a pixel size of (0.4, 0.4, 0.4) µm.
+        'voxels' to indicate pixel indices within the JRC2018_FEMALE
+        image volume, which has a pixel size of (0.38, 0.38, 0.38) µm.
         Default is microns.
 
     output_units (str) :
         The units you want points returned to you in. Same set of
         options as for `input_units`, except that the pixel size of the
-        output space, FANC, is (4.3, 4.3, 45) nm.
+        output space, the BANC, is (4, 4, 45) nm.
         Default is nanometers.
 
     reflect (bool) :
         Whether to reflect the point coordinates across the midplane of
-        the JRC2018_VNC_FEMALE template before warping them into
-        FANC-space. This reflection moves points' x coordinates from the
-        left to the right side of the VNC template or vice versa, but
+        the JRC2018_FEMALE template before warping them into
+        BANC-space. This reflection moves points' x coordinates from the
+        left to the right side of the brain template or vice versa, but
         does not affect their y coordinates (anterior-posterior axis) or
         z coordinates (dorsal-ventral axis).
         Default is False.
 
     Returns
     -------
-    An Nx3 numpy array representing x,y,z point coordinates in FANC, in
-    units specified by `output_units`.
-
-    ------
-    Originally published in https://www.lee.hms.harvard.edu/phelps-hildebrand-graham-et-al-2021
-      and made available in that paper's repository located at:
-      https://github.com/htem/GridTape_VNC_paper/tree/main/template_registration_pipeline/register_EM_dataset_to_template
-    This function is a slight improvement on the published version.
+    An Nx3 numpy array representing x,y,z point coordinates in the BANC,
+    in units specified by `output_units`.
     """
     # Only required for deprecated functions so not imported up top
     import transformix  # https://github.com/jasper-tms/pytransformix
 
     points = np.array(points)
     if len(points.shape) == 1:
-        result = warp_points_template_to_FANC(np.expand_dims(points, 0),
-                                             input_units=input_units,
-                                             output_units=output_units,
-                                             reflect=reflect)
+        result = warp_points_brain_template_to_BANC(
+            np.expand_dims(points, 0),
+            input_units=input_units,
+            output_units=output_units,
+            reflect=reflect
+        )
         if result is None:
             return result
         else:
@@ -316,29 +277,24 @@ def warp_points_template_to_FANC(points,
     if input_units == 'nanometers':
         points /= 1000  # Convert nm to microns
     elif input_units == 'voxels':
-        points = points * 0.4 # Convert voxels to microns
+        points = points * 0.38  # Convert voxels to microns
 
-    if not reflect:  # The z flip below will cause a reflection, so un-reflect
-        points[:, 0] = template_plane_of_symmetry_x_microns * 2 - points[:, 0]
+    if reflect:
+        points[:, 0] = brain_template_plane_of_symmetry_x_microns * 2 - points[:, 0]
 
     transform_params = os.path.join(
         os.path.dirname(__file__),
         'transform_parameters',
-        'TransformParameters.FixedTemplate.Bspline.txt'
+        'brain',
+        'template_to_BANC.txt',
     )
     # Do the transform. This requires input in microns and gives output in microns
     points = transformix.transform_points(points, transform_params)
 
-    points *= 1000  # Convert microns to nm so that the math below works
-    # This block of math looks mysterious but is explained in detail at
-    # https://github.com/htem/GridTape_VNC_paper/blob/main/template_registration_pipeline/register_EM_dataset_to_template/README.md
-    points[:, 2] = 435*400 - points[:, 2]  # z flipping a stack with 436 slices
-    points /= (300, 300, 400)
-    points *= (430, 430, 450)
-    points += (533.2, 533.2, 945)  # (1.24, 1.24, 2.1) vox at (430, 430, 450)nm/vox
+    points *= 1000  # Convert microns to nm
 
     if output_units == 'microns':
         points /= 1000  # Convert nm to microns
     elif output_units == 'voxels':
-        points /= (4.3, 4.3, 45)  # Convert nm to FANC voxels
+        points /= (4, 4, 45)  # Convert nm to BANC voxels
     return points
